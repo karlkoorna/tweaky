76dcbcca4ab8fb2a8ad402eb632f85d57ac6e784
Karl Köörna <karlkoorna@gmail.com>
2018-06-18 11:57:43 +0300

    Update metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("Tweaky")]
 [assembly: AssemblyProduct("Tweaky")]
-[assembly: AssemblyDescription("Windows 10 tweaker.")]
+[assembly: AssemblyDescription("A collection of useful tweaks for Windows 10.")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
