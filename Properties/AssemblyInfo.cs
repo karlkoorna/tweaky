c3eb54cfe58f28de9a8a967060f316d57e85f077
Karl Köörna <karlkoorna@gmail.com>
2019-09-03 20:43:02 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: AssemblyTitle("Tweaky")]
 [assembly: AssemblyProduct("Tweaky")]
 [assembly: AssemblyDescription("Windows 10 tweaker.")]
-[assembly: AssemblyCopyright("AGPL-3.0")]
+[assembly: AssemblyCopyright("Karl Köörna")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
