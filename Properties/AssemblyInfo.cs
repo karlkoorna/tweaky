b084be9dcee92c6ef18691d131248e2ba6e161e5
Karl Köörna <karlkoorna@gmail.com>
2020-07-11 17:49:25 +0300

    Upgrade to .NET Core 3.1

--- Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Tweaky")]
-[assembly: AssemblyProduct("Tweaky")]
-[assembly: AssemblyDescription("GUI for Windows tweaks.")]
-[assembly: AssemblyCopyright("Karl Köörna")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
